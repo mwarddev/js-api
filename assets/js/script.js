d34c4d96cea970adc8e32b49784ad31a9c7b6e14
mwarddev <martinward1981@googlemail.com>
2021-12-17 15:54:02 +0000

    Add api requset to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-nveyiwj4biG37dd1xg7x4M-Q3hI
+const API_KEY = "nveyiwj4biG37dd1xg7x4M-Q3hI";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
+}
